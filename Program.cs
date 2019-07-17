d9e6e9e3332f2e0d92e7110b39ce9dd41e5fffff
Anthony Golovin and Joseph Tubridy <anthony@>
2019-07-17 14:35:06 -0700

    allow us to add to or kind of print out our list

--- Program.cs
+++ Program.cs
@@ -8,14 +8,24 @@ namespace Wildpark.Animals {
   {
     public static void Main()
     {
-    
+    List<Animal> AnimalList = new List<Animal>();
+    AddAnimal(AnimalList);
+    }
+  
+    static void AddAnimal(List<Animal> AnimalList)
+    {
 
-      Console.WriteLine("Enter an animal for yout logbook: ");
+
+        Console.WriteLine("Would you like to add an animal? {y or n]");
+    string checkAdd = Console.ReadLine();
+    if (checkAdd.ToLower() == "y")
+    {
+       Console.WriteLine("Enter an animal for your logbook: ");
       Console.WriteLine("Enter the animal species");
       string inputSpecies = Console.ReadLine();
-      Console.WriteLine("Enter the animals diet type");
+      Console.WriteLine("Enter the animals diet type. [Carnivore, Omnivore, Vegetarian, Vegan]");
       string inputDietType = Console.ReadLine();
-      Console.WriteLine("Enter the animals class");
+      Console.WriteLine("Enter the animals class. [Mammal, Reptile, Marsupial, Bird, Monotreme]");
       string inputAnimalClass = Console.ReadLine();
       Animal newAnimal = new Animal(inputSpecies, inputDietType, inputAnimalClass); 
 
@@ -23,8 +33,35 @@ namespace Wildpark.Animals {
         Console.WriteLine(newAnimal.GetSpecies());
         Console.WriteLine("Is a " + newAnimal.GetDietType());
         Console.WriteLine("Is of the Class " + newAnimal.GetAnimalClass());
-        //Console.WriteLine(Car.MakeSound("Bang"));
-      List<Animal> Animals = new List<Animal>() {};  
+        
+      
+       AnimalList.Add(newAnimal);
+
+     
+    }
+    else if (checkAdd.ToLower() == "n")
+    {
+        OptionBranch(AnimalList);
+    }
+    else
+    {
+        Console.WriteLine("I don't think you understood");
+    }
+         AddAnimal(AnimalList);
+     
   }
+  static void OptionBranch(List<Animal> AnimalList)
+  {
+    //   Console.WriteLine("Would you like to see the animals we have? [y or n]");
+    //   string seeListCheck = Console.ReadLine();
+    //   if (seeListCheck.ToLower == "y")
+    //   {}
+
+    Console.WriteLine(AnimalList[0]);
+
+
+      Main();
+  }
+
   }
 }
